c3242729ed840cc226eddd89b011157c7559fc5b
znguyen <znguyen@getraline.com>
2023-09-18 06:38:52 +0200

    Use ImageUpload Service to upload image during registration

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -9,11 +9,9 @@ use App\Repository\TrickRepository;
 use App\Service\ImageUpload;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 class TrickController extends AbstractController
 {
